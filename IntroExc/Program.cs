ca9036757d7423b6f14d7a614a2d9b2043a141a2
Adam Kjellberg <76393729+AdamKj@users.noreply.github.com>
2021-08-18 10:01:33 +0200

    Initial commit

---
+++ IntroExc/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace IntroExc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Adam";
+
+            Console.WriteLine("Hello! My name is " + name + "!");
+
+            Console.ReadKey();
+        }
+
+    }
+}
